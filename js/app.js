190248f93df757d2a78ae3e3bdba15919e75be09
Viorel Robu <wwjd.vio@gmail.com>
2020-04-26 11:35:25 +0300

    refactor to Bible class

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
 const chapter = document.querySelector('#paragraph');
 
-async function getBible() {
-    try {
-        const bible = await axios.get('/bible/json/ro_cornilescu.json');
+class Bible {
+    constructor(bible, book = 0, chapter = 0) {
+        this.bible = bible;
+        this.book = book;
+        this.chapter = chapter;
+    }
+
+    load() {
+        return axios.get(`/bible/json/${this.bible}.json`);
+    }
+
+    async displayChapter() {
+        const stream = await this.load();
         chapter.innerHTML = '';
+        let verseNum;
         let i = 1;
-        for (let verseData of bible.data[0].chapters[0]) {
+        for (let verseData of stream.data[this.book].chapters[this.chapter]) {
             const verse = document.createElement('div');
             verse.classList.add('verse');
             verseNum = String(i);
-            verse.innerHTML = `${verseNum}). ${verseData}`;
+            verse.innerHTML = `${verseNum}. ${verseData}`;
             chapter.append(verse);
             i++;
         }
-    }catch(err) {
-        console.log('We got an error!', err);
+    }
+
+    change(book, chapter) {
+        this.book = book;
+        this.chapter = chapter;
+        this.displayChapter();
     }
 }
-getBible();
+
+const bible = new Bible('ro_cornilescu', 1, 2);
+bible.displayChapter();
